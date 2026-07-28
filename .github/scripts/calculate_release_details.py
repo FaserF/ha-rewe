77ec69e541d6cc8b722af93391d668f9e914499b
Fabian Seitz <fabian.seitz@pari.com>
2026-07-28 13:58:29 +0200

    ruff CI linter fix

--- .github/scripts/calculate_release_details.py
+++ .github/scripts/calculate_release_details.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3  # noqa: EXE001
 import glob
 import json
 import os
--- scripts/generate_changelog.py
+++ scripts/generate_changelog.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3  # noqa: EXE001
 """Generates a structured, deduplicated, user-friendly changelog from git commit history."""
 
 import argparse
